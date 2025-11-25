72f0352792947671a9c6cea172e56f15b47a28b6
LMSolera <lumi.solera@gmail.com>
2025-11-25 17:47:01 -0300

    Cliente do retrofit para gerência de requisições de api, e começo da implementação da classe de service que vai definir nossas interações com a API do banco em nuvem.

--- app/src/main/java/com/example/ddm_projetofinal/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/ddm_projetofinal/data/api/RetrofitClient.kt
@@ -1,4 +1,43 @@
 package com.example.ddm_projetofinal.data.api
 
+import com.example.ddm_projetofinal.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object RetrofitClient {
+    private const val BASE_URL = BuildConfig.SUPABASE_URL
+    private const val API_KEY = BuildConfig.SUPABASE_KEY
+
+    private val authInterceptor = Interceptor { chain ->
+        val newRequest = chain.request().newBuilder()
+            .addHeader("apikey", API_KEY)
+            .addHeader("Authorization", "Bearer $API_KEY")
+            .addHeader("Content-Type", "application/jason")
+            .build()
+        chain.proceed(newRequest)
+    }
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val supabaseService: SupabaseService = retrofit.create(SupabaseService::class.java)
 }
--- app/src/main/java/com/example/ddm_projetofinal/data/api/SupabaseService.kt
+++ app/src/main/java/com/example/ddm_projetofinal/data/api/SupabaseService.kt
@@ -1,4 +1,59 @@
 package com.example.ddm_projetofinal.data.api
 
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+
 interface SupabaseService {
+
+    @GET("rest/v1/tb_user")
+    suspend fun login (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @POST("rest/v1/tb_user")
+    suspend fun newUser (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @GET("rest/v1/tb_trip")
+    suspend fun getTrips (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @POST("rest/v1/tb_trip")
+    suspend fun insertTrip (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @PATCH("rest/v1/tb_trip")
+    suspend fun updateTrip (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @DELETE("rest/v1/tb_trips")
+    suspend fun deleteTrip (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @GET("rest/v1/tb_expense")
+    suspend fun getExpenses (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @POST("rest/v1/tb_expense")
+    suspend fun insertExpense (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @PATCH("rest/v1/tb_expense")
+    suspend fun updateExpense (
+        //TODO :: FULL IMPLEMENTATION
+    )
+
+    @DELETE("rest/v1/tb_expense")
+    suspend fun deleteExpense (
+        //TODO :: FULL IMPLEMENTATION
+    )
 }
